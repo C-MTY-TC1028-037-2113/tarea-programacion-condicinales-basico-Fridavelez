45334e2f7f1feab84a1a799791eebb596694aba2
Frida Velez <a00835434@tec.mx>
2021-09-22 17:27:11 -0500

    FR

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -5,14 +5,13 @@ def main():
    if edad >= 18:
       identificacion = (input("¿Tienes identificación oficial? (s/n): "))
       
-      if identificacion == "n":
-         print("No cumples con los requisitos")
-      
-      if identificacion == "s":
-         print("Tramite de licencia concedido")
+      if identificacion == ("n" or "N"):
+         print("No cumples requisitos")
+      elif identificacion == ("s" or "S"):
+         print("Trámite de licencia concedido")
              
    elif edad <=18:
-         print("No cumples los requisitos")
+         print("No cumples requisitos")
    else: 
          print("Respuesta incorrecta")
  

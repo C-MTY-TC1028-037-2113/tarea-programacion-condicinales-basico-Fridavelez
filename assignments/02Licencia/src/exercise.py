50fbd08b02a66fc458fe10eac699a55b33772202
Frida Velez <a00835434@tec.mx>
2021-09-22 15:53:15 -0500

    hola

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,21 +1,22 @@
 
 def main():
-    edad = int(input("Ingresa tu edad: "))
-    identificacion = str(input("Tienes identifecacion oficial s/n?: "))
+   edad = int(input("Ingresa tu edad: "))
+   
 
 
-    if edad >= 18:
-         if identificacion =="s":
-            print("Tramite de licencia concedido")
-         elif identificacion == "n":
-            print("No cumple los requisitos")
-         else:
-             print("respuesta incorrecta")        
-    elif edad <=18:
+   if edad >= 18:
+      identificacion = str(input("¿Tienes identificación oficial? (s/n): "))
+      if identificacion == "s" or "S":
+          print("Tramite de licencia concedido")
+      elif identificacion == "n" or "N":
+          print("No cumples los requisitos")
+      else:
+          print("Respuesta incorrecta")        
+   elif edad <=18:
        print("No cumples los requisitos")
-    else: 
-        print("respuesta incorrecta")
-
+   else: 
+        print("Respuesta incorrecta")
+ 
 
 if __name__ == '__main__':
     main()

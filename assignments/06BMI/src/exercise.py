ad0cd13cce3626a6d65eb4b70ea5067f1e223985
Frida Velez <a00835434@tec.mx>
2021-09-23 19:15:18 -0500

    co

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -3,7 +3,9 @@ def main():
  
  peso = float(input("Peso en kg: "))
  altura = float(input("Altura en m: "))
-
+ 
+ if peso <= 0:
+     print("Revisa tus datos, alguno de ellos es erróneo.")
  if altura <= 0:
     print("Revisa tus datos, alguno de ellos es erróneo.")
 
@@ -11,11 +13,11 @@ def main():
 
  if imc < 20 :
     print("PESO BAJO")
- elif imc < 25 :
+ elif imc >= 20 and imc <25 :
     print("NORMAL")
- elif imc < 30 :
+ elif imc >=25 and imc < 30 :
     print("SOBREPESO")
- elif imc < 35 :
+ elif imc >=30 and imc < 40 :
     print("OBESIDAD")
  elif imc < 40 :
     print("OBESIDAD MORBIDA")

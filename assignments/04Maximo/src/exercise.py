bf3afabf12afa8b12fa18fbc89325a70f009d340
Frida Velez <a00835434@tec.mx>
2021-09-28 14:57:51 -0500

    n

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,17 +1,10 @@
-def main():
-    #escribe tu código abajo de esta línea
+x = int(input("Ingresa el primer número: "))
+y = int(input("Ingresa el segundo número: "))
+z = int(input("Ingresa el tercer número: "))
 
-    x = int(input("Ingresa el primer número: "))
-    y = int(input("Ingresa el segundo número: "))
-    z = int(input("Ingresa el tercer número: "))
-
-    if x > z and x > y:
-        print(x)
-    elif y > x and y > z:
-        print(y)
-    elif z > y and z > x:
-        print(z)
-     
-if __name__=='__main__':
-    main()
-  
+if x > z and x > y:
+    print(x)
+elif y > x and y > z:
+    print(y)
+elif z > y and z > x:
+    print(z)

d72218744b5efbdcbbe81c1e2146febf29433775
Frida Velez <a00835434@tec.mx>
2021-09-28 14:39:21 -0500

    N

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,9 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
 
-    num1 = int(input("Ingresa el primer número:"))
-    num2 = int(input("Ingresa el segundo número:"))
-    num3 = int(input("Ingresa el tercer número:"))
+    num1 = int(input("Ingresa el primer número: "))
+    num2 = int(input("Ingresa el segundo número: "))
+    num3 = int(input("Ingresa el tercer número:", '15'))
 
     if num1 > num3 and num1 > num2:
         print(num1)

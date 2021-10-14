dff29e8125d33a40838c37188fd3cb90e1cf9e72
Frida Velez <a00835434@tec.mx>
2021-10-13 21:12:14 -0500

    comit

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -1,12 +1,16 @@
+def main():
+    # Escribe el código adecuado para completar el programa
 
+ num1 = int(input("Ingresa el primer número:"))
+ num2 = int(input("Ingresa el segundo número:"))
+ num3 = int(input("Ingresa el tercer número:"))
 
-x = int(input("Ingresa el primer número:"))
-y = int(input("Ingresa el segundo número:"))
-z = int(input("Ingresa el tercer número:"))
+ if num1 > num3 and num1 > num2:
+     print(num1)
+ elif num2 > num1 and num2 > num3:
+     print(num2)
+ elif num3 > num2 and num3 > num1:
+     print(num3) 
 
-if x > z and x > y:
-    print(x)
-elif y > x and y > z:
-    print(y)
-elif z > y and z > x:
-    print(z) 
+if __name__ == '__main__':
+    main()
